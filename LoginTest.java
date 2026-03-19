3e6a1824b515aceda1e83fc1ccbf9d483fa17781
nidhiim9-netizen <nidhiim9@gmail.com>
2026-03-19 16:14:16 +0530

    Rename class and update print statement in LoginTest
    Rename class and update print statement in LoginTest

---
+++ LoginTest.java
@@ -0,0 +1,7 @@
+class LoginTest
+  {
+    public static void main(String args[])
+    {
+      System.out.println("login with username and password in remote repoisitory GitHub");
+      }
+  }
--- MyStashExamp.java
+++
@@ -1,7 +0,0 @@
-class MyStashExamp
-  {
-    public static void main(String args[])
-    {
-      System.out.println("this is stash example created in remote repoisitory GitHub");
-      }
-  }
